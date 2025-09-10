edccc4f33a7c410a50ba0a0167cb0b9a48f05fbf
yash.fern99 <yash.fern99@gmail.com>
2025-09-10 10:33:46 +0530

    Changed folder strcture

---
+++ salon-service/src/main/java/com/yashodh/salon/service/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.yashodh.salon.service.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "salon microservice for salon booking system";
+
+    }
+}
---
+++ salon-service/src/main/java/com/yashodh/salon/service/service/SalonService.java
@@ -0,0 +1,16 @@
+package com.yashodh.salon.service.service;
+
+import com.yashodh.salon.service.dto.SalonDTO;
+import com.yashodh.salon.service.model.Salon;
+
+public interface SalonService {
+
+    Salon createSalon(SalonDTO salon, UserDTO user);
+
+
+    dvxvxv
+
+
+
+
+}
