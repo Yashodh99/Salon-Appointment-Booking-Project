bf610e9e98a95289fd848d8e009289fce49bfb39
yash.fern99 <yash.fern99@gmail.com>
2025-09-11 21:38:26 +0530

    Added Category Microservices to the folder structure

---
+++ category-service/src/main/java/com/yashodh/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.yashodh.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+
+    @GetMapping
+    public String HomeControllerHandler(){
+        return "Category microservice for salon booking system";
+
+    }
+}
